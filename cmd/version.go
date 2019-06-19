87474a7aa0db781fa311a84dc3cb37db62354be3
d-kuro <kurosawa7620@gmail.com>
2019-06-19 19:40:51 +0900

    Add logger.

--- cmd/version.go
+++ cmd/version.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -15,11 +16,11 @@ func NewVersionCmd(o *RestartOptions) *cobra.Command {
 		Use:   "version",
 		Short: "Show version",
 		Run: func(cmd *cobra.Command, args []string) {
-			versionCmd(o)
+			runVersionCmd()
 		},
 	}
 }
 
-func versionCmd(o *RestartOptions) {
-	fmt.Fprintf(o.outWriter, "version: %s (rev: %s)\n", Version, Revision)
+func runVersionCmd() {
+	fmt.Fprintf(os.Stdout, "version: %s (rev: %s)\n", Version, Revision)
 }
